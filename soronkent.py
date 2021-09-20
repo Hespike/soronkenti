1af74e9352ba8436f1b1ba22c5005097a2af66f7
Sárdi András <sardi.andras@gmail.com>
2021-09-20 09:06:32 +0200

    Create soronkent.py

---
+++ soronkent.py
@@ -0,0 +1,7 @@
+#Írj egy programot, ami bekéri a felhasználó adatait változóban, majd soronként kiírja azt!
+
+a = input("Elso adat: ")
+b = input("Masodik adat: ")
+
+
+print('A kapott adatok:\n' + a + '\n' + b)
